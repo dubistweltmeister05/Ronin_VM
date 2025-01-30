207ab079871eeaedfa0e4df98f56d9eb197700da
Kshitij Neeraj Vaze <kshitijvaze@gmail.com>
2025-01-30 07:56:17 +0530

    Add files via upload

---
+++ rvm.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef enum
+{
+	INST_PUSH,
+	INST_POP,
+	INST_ADD,
+	INST_SUB,
+	INST_PRINT,
+} Inst_set;
+
+typedef struct
+{
+	Inst_set type;
+	int value;
+} Inst;
+
+Inst program[] = {
+	{.type =INST_PUSH, .value=15 },
+	{.type=INST_PRINT},
+};
+
+#define PROGRAM_SIZE sizeof(program) / sizeof(program[0])
+#define MAX_STACK_SIZE 1024
+int stack[MAX_STACK_SIZE];
+int stack_size;
+
+
+
+void push(int value){
+	stack[stack_size++]=value;
+}
+
+
+int pop(){
+return stack[--stack_size];
+}
+
+int main()
+{
+
+	for (size_t ip = 0; ip < PROGRAM_SIZE; ip++)
+	{
+		switch (program[ip].type)
+		{
+		case INST_PUSH:
+			push(program[ip].value);
+			break;
+		case INST_POP:
+			break;
+		case INST_ADD:
+			break;
+		case INST_SUB:
+			break;
+		case INST_PRINT:
+			printf("%d\n", pop());
+			break;
+		
+		}
+	}
+
+	return 0;
+}
